09fd57e5620ba4d8d1f3d15909f1310e5aaf2f5f
Jan Cloud Silva Andarde <1-002614@abefs.intranet>
2017-11-03 10:20:54 -0300

    Criando a View de Usuário
    criei a parte mais simples da view de usuário, somente tela. ainda não fiz as logicas em sí! nem settei nome aos componentes

---
+++ KeroPrecoAdmin/src/keroprecoadmin/controllers/UsuarioFXMLController.java
@@ -0,0 +1,17 @@
+
+package keroprecoadmin.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class UsuarioFXMLController implements Initializable {
+
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+    
+}
